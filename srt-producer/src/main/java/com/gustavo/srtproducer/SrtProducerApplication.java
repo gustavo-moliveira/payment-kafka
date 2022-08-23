70a4a87d053ae14d472aadd957074d37f198f0a1
Gustavo Moraes <gustavo.moliveira@bandtec.com.br>
2022-08-23 15:05:03 -0300

    str-producer started

---
+++ srt-producer/src/main/java/com/gustavo/srtproducer/SrtProducerApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.srtproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrtProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrtProducerApplication.class, args);
+    }
+
+}
---
+++ srt-producer/src/test/java/com/gustavo/srtproducer/SrtProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.srtproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrtProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
