6b8f7cb6dd24c17572f4d5e33baa259f69b74965
Gustavo Moraes <gustavo.moliveira@bandtec.com.br>
2022-08-24 13:27:14 -0300

    started payment project

---
+++ payment-service/src/main/java/com/gustavo/paymentservice/resouce/PaymentResource.java
@@ -0,0 +1,11 @@
+package com.gustavo.paymentservice.resouce;
+
+import com.gustavo.paymentservice.model.Payment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface PaymentResource {
+    @PostMapping
+    ResponseEntity<Payment> payment(@RequestBody Payment payment);
+}
